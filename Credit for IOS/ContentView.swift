c258c12e686bff2d6850325a6bdcce12cc61d946
surgosan <surgosan@gmail.com>
2024-07-08 19:38:10 -0400

    Init Credit Tracker

--- Credit for IOS/ContentView.swift
+++ Credit for IOS/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "dollarsign")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//                .foregroundStyle(.tint)
+            Text("Credit Finances Tracker")
         }
         .padding()
     }
