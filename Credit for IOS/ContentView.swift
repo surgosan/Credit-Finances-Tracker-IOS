9c9f1b3b6dafc6f1e690c4473fed0a2f10bb8364
surgosan <surgosan@gmail.com>
2024-07-08 19:36:23 -0400

    Initial Commit

---
+++ Credit for IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Credit for IOS
+//
+//  Created by Sergio Sanchez-Alvares on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Credit for IOS/Credit_for_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Credit_for_IOSApp.swift
+//  Credit for IOS
+//
+//  Created by Sergio Sanchez-Alvares on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Credit_for_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
